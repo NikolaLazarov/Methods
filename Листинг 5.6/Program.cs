281d6af86a500384ce6a8842606d6d436e9d2fb4
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2021-12-16 22:32:53 +0200

    2/3

---
+++ Листинг 5.6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_5._6
+{
+    class Program
+    {
+        static void alpha(ref int n)
+        {
+            WriteLine("В метод alpha(). На вход," + n);
+            n++;
+            WriteLine("В метод alpha(). На изход," + n);
+        }
+        static void barvo(ref int[] n)
+        {
+            WriteLine("В метод bravo(). На вход:" + ArrayToText(n));
+            for (int i = 0; i < n.Length; i++)
+            {
+                n[i]++;
+            }
+            WriteLine("В метод bravo(). На изход: " + ArrayToText(n));
+        }
+        static void charlie(ref int[] n)
+        {
+            WriteLine("В метод charlie(). На вход: " + ArrayToText(n));
+            int[] m = new int[n.Length];
+            for (int i = 0; i < n.Length; i++)
+            {
+                m[i] = n[i] + 1;
+            }
+            n = m;
+            WriteLine("В метод charlie(). На изход: " + ArrayToText(n));
+        }
+        static string ArrayToText(int[] n)
+        {
+            string res = "[" + n[0];
+            for (int i = 1; i < n.Length; i++)
+            {
+                res += "," + n[i];
+            }
+            res += "]";
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            int A = 100;
+
+            WriteLine("До извикване на метода alpha: A = " + A);
+            alpha(ref A);
+            WriteLine("След извъкване на метода alpha: A = " + A);
+
+            int[] B = { 1, 3, 5 };
+            WriteLine("До извикване на метода bravo() :B = " + ArrayToText(B));
+            barvo(ref B);
+            WriteLine("След извикване на метода bravo() :B = " + ArrayToText(B));
+
+            int[] C = { 2, 4, 6 };
+            WriteLine("До извикване на метод charlie(): C = " + ArrayToText(C));
+            charlie(ref C);
+            WriteLine("След извикване на метода charlie(): C = " + ArrayToText(C));
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Листинг 5.7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_5._7
+{
+    class Program
+    {
+        static int getMin(int[]nums,out int index){
+            index = 0;
+            for(int k = 1;k<nums.Length;k++){
+                if(nums[k]<nums[index]){
+                    index = k;
+                }
+            }
+            return nums[index];
+        }
+        static void Main(string[] args)
+        {
+            int[] A = {24,10,7,18,34,8,12,90};
+            foreach(int v in A){
+                Write("| {0} ",v);
+            }
+            WriteLine("|");
+
+            int val,k;
+            val=getMin(A,out k);
+            WriteLine("Най-малката стойност : " + val);
+            WriteLine("Индекс на елемента : "+k);
+            WriteLine("Проверка : A[{0}]={1}",k,A[k]);
+
+            
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}

91bbf271f1bf4a26e64f07f695a5e61f50110065
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-06 21:35:36 +0200

    Add files via upload

---
+++ Самостоятели задачи/Задача 1/Задача 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5;
+            sumofNums(A);
+
+            WriteLine("Press nay key to exit...");
+            ReadKey(true);
+        }
+        static void sumofNums(int n)
+        {
+            int k = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                k += i * i;
+            }
+            WriteLine(k);
+        }
+    }
+}

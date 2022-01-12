de3f059524137e81b3fb87780c137cdd6af42fda
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-12 20:33:38 +0200

    Add files via upload

---
+++ Задача 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] A = { 9, 3, 5, 1, 10 };
+            Meth(A);
+            WriteLine("Press any key to exit");
+            ReadKey(true);
+        }
+        static void Meth(double[] arrNums)
+        {
+            double var = 0;
+            for (int i = 0; i < arrNums.Length; i++)
+            {
+                var += arrNums[i];
+            }
+            WriteLine(var / arrNums.Length);
+        }
+    }
+}

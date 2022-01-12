59987f986b591f6c112a0a817d795e4177ec7780
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-12 17:59:01 +0200

    1/2

---
+++ Самостоятели задачи/Задача 1/Задача 3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[3];
+            Random rnd = new Random();
+            for (int i = 0; i < A.Length; i++)
+            {
+                int k = rnd.Next(0, 9);
+                A[i] = k;
+                Write(A[i]);
+            }
+            WriteLine();
+            int B = 5;
+            rndformArr(A, B);
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+        static void rndformArr(int[] numsArr, int n)
+        {
+            int[] xyz = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (n > numsArr.Length)
+                {
+                    int[] newArr = new int[n];
+                    int p = 0;
+                    for (int j = 0; j < n; j++)
+                    {
+                        newArr[j] = numsArr[p];
+                        p++;
+                        if (p == numsArr.Length)
+                        {
+                            p = 0;
+                        }
+                    }
+                    Write(newArr[i]);
+                }
+                else
+                {
+                    xyz[i] = numsArr[i];
+                    Write(xyz[i]);
+                }
+            }
+            WriteLine();
+        }
+    }
+}
---
+++ Самостоятели задачи/Задача 1/Задача 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] A = {'A','B','C'};
+            Meth(A);
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+        static void Meth(char[] arrSymbs){
+            foreach(var i in arrSymbs){
+                WriteLine((int)i);
+            }
+        }
+    }
+}

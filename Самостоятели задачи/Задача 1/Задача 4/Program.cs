788f3e5aaa09be582184b5e15e47a2a802abdec1
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-15 21:11:40 +0200

    Add files via upload

---
+++ Самостоятели задачи/Задача 1/Задача 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void meth(char[] arrSymbol)
+        {
+            int[] arrCode = new int[arrSymbol.Length];
+            for (int i = 0; i < arrCode.Length; i++)
+            {
+                arrCode[i] = (int)arrSymbol[i];
+                Write(arrCode[i]+" ");
+            }
+            WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            char[] A = { 'A', 'B', 'C' };
+            meth(A);
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+    }
+}

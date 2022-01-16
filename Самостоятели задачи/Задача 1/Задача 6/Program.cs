e14d7ff08a7cb99fdd4c4399bc4df1f58fcf6a48
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-16 16:43:54 +0200

    2/3

---
+++ Самостоятели задачи/Задача 1/Задача 6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace Задача_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrNums = new int[2, 3];
+            Random rnd = new Random();
+            WriteLine("The array: ");
+            for (int i = 0; i < arrNums.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrNums.GetLength(1); j++)
+                {
+                    arrNums[i, j] = rnd.Next(1, 9);
+                    Write(arrNums[i, j] + " ");
+                }
+                WriteLine();
+            }
+
+            Meth(arrNums);
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+        static void Meth(int[,] arrNums)
+        {
+            int min, max;
+            max = arrNums[0, 0];
+            int y = 0, x = 0;
+            for (int i = 0; i < arrNums.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrNums.GetLength(1); j++)
+                {
+                    if (arrNums[i, j] > max)
+                    {
+                        max = arrNums[i, j];
+                        y = i;
+                        x = j;
+                    }
+                }
+            }
+            WriteLine("The max value: " + max);
+            WriteLine("Index of max value: " + "[" + x + ", " + y + "]");
+        }
+    }
+}
---
+++ Самостоятели задачи/Задача 1/Задача 7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] babaGanq = { 1, 2, 3, 4, 5, 6 };
+            WriteLine("The array before: ");
+            foreach (int i in babaGanq)
+            {
+                Write(i + " ");
+            }
+            WriteLine();
+
+            WriteLine("The array after: ");
+            pechki(babaGanq);
+
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+        static void pechki(int[] babaGanq)
+        {
+            int temp = 0;
+            for (int i = 0; i < babaGanq.Length / 2; i++)
+            {
+                temp = babaGanq[i];
+                babaGanq[i] = babaGanq[babaGanq.Length - (i + 1)];
+                babaGanq[babaGanq.Length - (i + 1)] = temp;
+            }
+            foreach (int i in babaGanq)
+            {
+                Write(i + " ");
+            }
+            WriteLine();
+        }
+    }
+}

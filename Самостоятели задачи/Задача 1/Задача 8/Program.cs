e78070c4d43427eb54847af1ed04058407f1959c
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-16 21:38:03 +0200

    Add files via upload

---
+++ Самостоятели задачи/Задача 1/Задача 8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meth(1, 8);
+            Meth('A', 'H');
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+        static void Meth(int a, int b)
+        {
+            int[] arrNums = new int[b];
+            arrNums[0] = a;
+            for (int i = 1; i < arrNums.Length - 1; i++)
+            {
+                arrNums[i] = i + a;
+            }
+            arrNums[b - 1] = b;
+            foreach (int i in arrNums)
+            {
+                Write(i + " ");
+            }
+            WriteLine();
+        }
+        static void Meth(char a, char b)
+        {
+            int n = (int)b - 64;
+            char[] arrSymbs = new char[n];
+            arrSymbs[0] = a;
+            for (int i = 1; i < arrSymbs.Length-1; i++)
+            {
+                arrSymbs[i] = (char)(i + 65);
+            }
+            arrSymbs[n-1] = b;
+            foreach (char i in arrSymbs)
+            {
+                Write(i + " ");
+            }
+            WriteLine();
+        }
+    }
+}
---
+++ Самостоятели задачи/Задача 1/Задача 9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace Задача_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meth(69,5,13,25,30,20,99);
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+        static void Meth(params int[]refs){
+            int[] minMax = new int[2];
+            minMax[0]=refs.Min();
+            minMax[1]=refs.Max();
+            foreach(int i in minMax){
+                Write(i+" ");
+            }
+            WriteLine();
+        }
+    }
+}

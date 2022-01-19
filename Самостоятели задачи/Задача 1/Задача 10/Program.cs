88823b69ede0c5c328ac6900a656d530499cc16e
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-19 22:26:07 +0200

    Add files via upload

---
+++ Самостоятели задачи/Задача 1/Задача 10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meth("Maika ti e ",'P','E','C','H','K','A');
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+        static void Meth(string txt, params char[]arrSymbs){
+            arrSymbs.ToString();
+            for(int i =0;i<arrSymbs.Length;i++){
+                txt += arrSymbs[i];
+            }
+            WriteLine(txt);
+        }
+    }
+}
